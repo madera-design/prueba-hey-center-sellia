0557d62b9a0322aa53e9a1d5770ba14c16664311
jose madera <josemadera289@gmail.com>
2022-08-21 19:44:48 -0500

    subir archivos

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<!-- eslint-disable --> 
+<template>
+  <div id="app" class="container-fluid c__light">
+    <div class="h-menu">
+      <div class="h-menu--logo">
+        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 71.519 71.519">
+              <g id="Grupo_56" data-name="Grupo 56" transform="translate(-21.7 -46.74)">
+                  <path id="message-circle" d="M72.519,35.829A32.365,32.365,0,0,1,69.043,50.5,32.829,32.829,0,0,1,39.691,68.657a32.365,32.365,0,0,1-14.676-3.476L3,72.519,10.338,50.5A32.365,32.365,0,0,1,6.862,35.829,32.829,32.829,0,0,1,25.014,6.476,32.365,32.365,0,0,1,39.691,3h1.931a32.751,32.751,0,0,1,30.9,30.9Z" transform="translate(19.7 44.74)" fill="#fff" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                  <text id="HEY_" data-name="HEY!" transform="translate(40 87)" fill="#014157" font-size="18" font-family="OpenSans-Bold, Open Sans" font-weight="700"><tspan x="0" y="0">HEY!</tspan></text>
+              </g>
+          </svg>
+      </div>
+      <div class="h-menu--opciones">
+          <ul class="h-menu--opciones__ul">
+              <li class="h-menu--opciones__li">
+                  <svg class="svg-icon" width="30" height="30">
+                    <use xlink:href="/img/icons.svg#home"></use>
+                  </svg>
+              </li>
+              <li class="h-menu--opciones__li">
+                 <svg class="svg-icon" width="30" height="30">
+                    <use xlink:href="/img/icons.svg#star"></use>
+                  </svg>
+              </li>
+              <li class="h-menu--opciones__li active">
+                  <svg class="svg-icon" width="30" height="30">
+                    <use xlink:href="/img/icons.svg#msg"></use>
+                  </svg>
+              </li>
+              <li class="h-menu--opciones__li">
+                  <svg class="svg-icon" width="30" height="30">
+                    <use xlink:href="/img/icons.svg#graf"></use>
+                  </svg>
+              </li>
+              <li class="h-menu--opciones__li">
+                  <svg class="svg-icon" width="30" height="30">
+                    <use xlink:href="/img/icons.svg#config"></use>
+                  </svg>
+              </li>
+              <li class="h-menu--opciones__li">
+                <svg class="svg-icon" width="30" height="30">
+                  <use xlink:href="/img/icons.svg#luna"></use>
+                </svg>
+            </li>
+          </ul>
+
+      </div>
+      <div class="h-menu-avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 80 80">
+              <g id="Grupo_44" data-name="Grupo 44" transform="translate(-33 -14)">
+                  <circle id="Elipse_3" data-name="Elipse 3" cx="40" cy="40" r="40" transform="translate(33 14)" fill="#00cbcc"/>
+                  <g id="user" transform="translate(47 29.875)">
+                  <path id="Trazado_2" data-name="Trazado 2" d="M46,30.75V25.5A10.5,10.5,0,0,0,35.5,15h-21A10.5,10.5,0,0,0,4,25.5v5.25" transform="translate(0 19.5)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/>
+                  <circle id="Elipse_8" data-name="Elipse 8" cx="10.5" cy="10.5" r="10.5" transform="translate(15.118 3)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/>
+                  </g>
+              </g>
+          </svg>
+      </div>
+    </div>
+    <div class="main">
+      <Bodymsg />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Bodymsg from './components/Bodymsg.vue';
+export default {
+  name: 'App',
+  components: {
+    Bodymsg
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/sass/style.scss";
+</style>
---
+++ src/components/Bodymsg.vue
@@ -0,0 +1,303 @@
+<!-- eslint-disable --> 
+<template>
+  <div class="contenedor-mensajes">
+    <div class="row">
+      <div class="col-4">
+        <div class="c-listaContactos">
+          <div class="row">
+            <div class="col-12">
+              <input type="text" placeholder="Buscar" class="c-search w-100">
+            </div>
+            <div class="pt-5 col-12">
+              <div class="lista-contactos">
+                <h3 class="lista-contactos__h3">Chats</h3>
+                <div class="conteneder-contactos">
+                  <div class="mb-3 d-flex align-items-center user-active">
+                    <div class="col-3 text-center">
+                      <svg class="svg-icon" width="60" height="60" >
+                        <use xlink:href="/img/icons.svg#user" ></use>
+                      </svg>
+                    </div>
+                    <div class="info-usuario p-2">
+                      <div class="nombre-usuario">
+                        <p class="txt-usuario">lorem impsun</p>
+                        <p class="txt-fecha">10-02-202</p>
+                      </div>
+                      <div class="texto-usuario">
+                        Lorem Ipsum is simply dummy text of the printing and...
+                      </div>
+                      <div class="notif-usuario">
+                        <div class="d-flex align-items-center justify-content-end">
+                          <p class="txt-notif">visto</p>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="11.414" height="8.207" viewBox="0 0 11.414 8.207">
+                            <path id="check" d="M14,6,7.125,13,4,9.818" transform="translate(-3.293 -5.293)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3 d-flex align-items-center user-disable">
+                    <div class="col-3 text-center">
+                      <svg class="svg-icon" width="60" height="60" >
+                        <use xlink:href="/img/icons.svg#userdisable" ></use>
+                      </svg>
+                    </div>
+                    <div class="info-usuario p-2">
+                      <div class="nombre-usuario">
+                        <p class="txt-usuario">lorem impsun</p>
+                        <p class="txt-fecha">10-02-202</p>
+                      </div>
+                      <div class="texto-usuario">
+                        Lorem Ipsum is simply dummy text of the printing and...
+                      </div>
+                      <div class="notif-usuario">
+                        <div class="d-flex align-items-center justify-content-end">
+                          <p class="txt-notif">visto</p>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="11.414" height="8.207" viewBox="0 0 11.414 8.207">
+                            <path id="check" d="M14,6,7.125,13,4,9.818" transform="translate(-3.293 -5.293)" fill="none" stroke="#888" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3 d-flex align-items-center  user-disable">
+                    <div class="col-3 text-center">
+                      <svg class="svg-icon" width="60" height="60" >
+                        <use xlink:href="/img/icons.svg#userdisable" ></use>
+                      </svg>
+                    </div>
+                    <div class="info-usuario p-2">
+                      <div class="nombre-usuario">
+                        <p class="txt-usuario">lorem impsun</p>
+                        <p class="txt-fecha">10-02-202</p>
+                      </div>
+                      <div class="texto-usuario">
+                        Lorem Ipsum is simply dummy text of the printing and...
+                      </div>
+                      <div class="notif-usuario">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 25 25">
+                          <g id="Grupo_43" data-name="Grupo 43" transform="translate(-560.25 -969)">
+                            <circle id="Elipse_18" data-name="Elipse 18" cx="12.5" cy="12.5" r="12.5" transform="translate(560.25 969)" fill="#2898ee"/>
+                            <text id="_3" data-name="3" transform="translate(568.198 986)" fill="#fff" font-size="16" font-family="OpenSans-Regular, Open Sans"><tspan x="0" y="0">3</tspan></text>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3 d-flex align-items-center  user-disable">
+                    <div class="col-3 text-center">
+                      <svg class="svg-icon" width="60" height="60" >
+                        <use xlink:href="/img/icons.svg#userdisable" ></use>
+                      </svg>
+                    </div>
+                    <div class="info-usuario p-2">
+                      <div class="nombre-usuario">
+                        <p class="txt-usuario">lorem impsun</p>
+                        <p class="txt-fecha">10-02-202</p>
+                      </div>
+                      <div class="texto-usuario">
+                        Lorem Ipsum is simply dummy text of the printing and...
+                      </div>
+                      <div class="notif-usuario">
+                        <div class="d-flex align-items-center justify-content-end">
+                          <p class="txt-notif">visto</p>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="11.414" height="8.207" viewBox="0 0 11.414 8.207">
+                            <path id="check" d="M14,6,7.125,13,4,9.818" transform="translate(-3.293 -5.293)" fill="none" stroke="#888" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3 d-flex align-items-center  user-disable">
+                    <div class="col-3 text-center">
+                      <svg class="svg-icon" width="60" height="60" >
+                        <use xlink:href="/img/icons.svg#userdisable" ></use>
+                      </svg>
+                    </div>
+                    <div class="info-usuario p-2">
+                      <div class="nombre-usuario">
+                        <p class="txt-usuario">lorem impsun</p>
+                        <p class="txt-fecha">10-02-202</p>
+                      </div>
+                      <div class="texto-usuario">
+                        Lorem Ipsum is simply dummy text of the printing and...
+                      </div>
+                      <div class="notif-usuario">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 25 25">
+                          <g id="Grupo_43" data-name="Grupo 43" transform="translate(-560.25 -969)">
+                            <circle id="Elipse_18" data-name="Elipse 18" cx="12.5" cy="12.5" r="12.5" transform="translate(560.25 969)" fill="#2898ee"/>
+                            <text id="_3" data-name="3" transform="translate(568.198 986)" fill="#fff" font-size="16" font-family="OpenSans-Regular, Open Sans"><tspan x="0" y="0">3</tspan></text>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-8">
+        <div class="">
+        <div class="row">
+          <div class="col-12">
+            <div class="row contacto">
+              <div class="col-9">
+                <div class=" d-flex align-items-center">
+                  <div class="text-center">
+                    <svg class="svg-icon" width="60" height="60" >
+                      <use xlink:href="/img/icons.svg#userdisable1" ></use>
+                    </svg>
+                  </div>
+                  <div class="info-usuario__chat">
+                    <p class="txt-usuario__chat">lorem impsun</p>
+                    <p class="txt-fecha__chat">312-000-0000</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-3 c-opcionesChat">
+                <svg class="svg-icon" width="18" height="18">
+                  <use xlink:href="/img/icons.svg#layout"></use>
+                </svg>
+                <svg class="svg-icon" width="18" height="18">
+                  <use xlink:href="/img/icons.svg#lupa"></use>
+                </svg>
+                <svg class="svg-icon" width="18" height="18">
+                  <use xlink:href="/img/icons.svg#burgermenu"></use>
+                </svg>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-12 c-mensajes">
+            <div class="c-mensajes__chats">
+              <p class="i-textchat">14 julio 2022</p>
+              <div class="c-chatenviado">
+                <div class="contendiochat">
+                  Lorem Ipsum is simply dummy text of the  printing and dummy text of the  printing
+                </div>
+                <div class="info-contendiochat">
+                  <div class="d-flex align-items-center justify-content-end">
+                    <p class="txt-info-contendiochat">visto</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.414" height="8.207" viewBox="0 0 11.414 8.207">
+                      <path id="check" d="M14,6,7.125,13,4,9.818" transform="translate(-3.293 -5.293)" fill="none" stroke="#888" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+                    </svg>
+                    <p class="txt-info-contendiochat">22:01</p>
+                  </div>
+                </div>
+              </div>
+              <div class="c-chatrecibido--contenedor">
+                <div  class="c-chatrecibido">
+                   <svg class="svg-icon" width="30" height="30" >
+                  <use xlink:href="/img/icons.svg#userdisable1" ></use>
+                </svg>
+                <div class="contendiochat--recibido">
+                  Lorem Ipsum is simply dummy text of the  printing and dummy text of the  printing
+                </div>
+                </div>
+                <p class="txt-info--recibido">22:01</p>
+              </div>
+              <div class="c-chatenviado">
+                <div class="contendiochat">
+                  Lorem Ipsum is simply dummy text of the  printing and dummy text of the  printing
+                </div>
+                <div class="info-contendiochat">
+                  <div class="d-flex align-items-center justify-content-end">
+                    <p class="txt-info-contendiochat">visto</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.414" height="8.207" viewBox="0 0 11.414 8.207">
+                      <path id="check" d="M14,6,7.125,13,4,9.818" transform="translate(-3.293 -5.293)" fill="none" stroke="#888" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+                    </svg>
+                    <p class="txt-info-contendiochat">22:01</p>
+                  </div>
+                </div>
+              </div>
+              <div class="c-chatrecibido--contenedor">
+                <div  class="c-chatrecibido">
+                   <svg class="svg-icon" width="30" height="30" >
+                  <use xlink:href="/img/icons.svg#userdisable1" ></use>
+                </svg>
+                <div class="contendiochat--recibido">
+                  Lorem Ipsum is simply dummy text of the  printing and dummy text of the  printing
+                </div>
+                </div>
+                <p class="txt-info--recibido">22:01</p>
+              </div>
+              <p class="i-textchat">la conversación cambio de "Resuelta" a "En proceso" 15 julio 2022 20:25</p>
+              <div class="c-chatenviado">
+                <div class="contendiochat">
+                  Lorem Ipsum is simply dummy text of the  printing and dummy text of the  printing
+                </div>
+                <div class="info-contendiochat">
+                  <div class="d-flex align-items-center justify-content-end">
+                    <p class="txt-info-contendiochat">visto</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.414" height="8.207" viewBox="0 0 11.414 8.207">
+                      <path id="check" d="M14,6,7.125,13,4,9.818" transform="translate(-3.293 -5.293)" fill="none" stroke="#888" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+                    </svg>
+                    <p class="txt-info-contendiochat">22:01</p>
+                  </div>
+                </div>
+              </div>
+              <div class="c-chatrecibido--contenedor">
+                <div  class="c-chatrecibido">
+                   <svg class="svg-icon" width="30" height="30" >
+                  <use xlink:href="/img/icons.svg#userdisable1" ></use>
+                </svg>
+                <div class="contendiochat--recibido">
+                  Lorem Ipsum is simply dummy text of the  printing and dummy text of the  printing
+                </div>
+                </div>
+                <p class="txt-info--recibido">22:01</p>
+              </div>
+            </div>
+            <div class="c-mensajes__enviar py-2">
+              <div class="row">
+                <div class="col-10">
+                  <span class="relative">
+                    <input type="text" class="i-enviarMsg w-100" placeholder="Escribe un mensaje" />
+                    <span class="absolute">
+                      <svg class="svg-icon" width="15" height="15">
+                        <use xlink:href="/img/icons.svg#adjuntar"></use>
+                      </svg>
+                    </span>
+                    <span class="absolute1">
+                      <svg class="svg-icon" width="15" height="15">
+                        <use xlink:href="/img/icons.svg#smile"></use>
+                      </svg>
+                    </span>
+
+                  </span>
+                  
+                </div>
+                <div class="col-2">
+                  <button class="btn btn-enviar">
+                    Enviar
+                    <svg class="svg-icon" width="15" height="15">
+                      <use xlink:href="/img/icons.svg#enviar"></use>
+                    </svg>
+                  </button>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
